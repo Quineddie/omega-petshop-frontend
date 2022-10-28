c41e245267f00cb7b5373fc8e27d0a8e9b0dbd5e
Eddie Quintero <92959645+Quineddie@users.noreply.github.com>
2022-10-28 15:37:44 -0500

    actualización carpetas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import ListaFormCategorias from "./componentes/categorias/ListaFormCategorias";
 import Header from "./componentes/general/Header";
 import Tarjetas from "./componentes/general/Tarjetas";
 import FormProducto from "./componentes/productos/FormProductos";
+import Banner from "./componentes/general/Baner";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () =>{
@@ -10,6 +11,7 @@ const App = () =>{
       <Header></Header>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Banner />} exact></Route>
           <Route path="/" element={<Tarjetas/>} exact></Route>
           <Route path="/categorias" element={<ListaFormCategorias/>} exact></Route>
           <Route path="/productos/form" element={<FormProducto/>} exact></Route>
