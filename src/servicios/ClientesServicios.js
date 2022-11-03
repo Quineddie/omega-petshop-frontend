6914e84493c5ad7b256c3dd50f47ab150b8e9170
Eddie Quintero <92959645+Quineddie@users.noreply.github.com>
2022-11-03 17:50:44 -0500

    actualizando frontend

--- src/servicios/ClientesServicios.js
+++ src/servicios/ClientesServicios.js
@@ -3,11 +3,11 @@ import axios from "axios";
 const ClientesServicios = {};
 
 ClientesServicios.listarClientes = () => {
-    return axios.get("http://localhost:8080/clientes");
+    return axios.get("http://localhost:8080/api/clientes");
 }
 
 ClientesServicios.buscarClientes = (criterio) => {
-    return axios.get("http://localhost:8080/clientes?q="+criterio);
+    return axios.get("http://localhost:8080/api/clientes?q="+criterio);
 }
 
 export default ClientesServicios;
